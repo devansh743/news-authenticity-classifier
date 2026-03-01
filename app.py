41b47f27549fd146726b6224d7e1fd8f4beb77ac
devansh743 <langaliyadevansh68@gmail.com>
2026-03-01 09:50:02 +0530

    Prepared for deployment

--- app.py
+++ app.py
@@ -308,8 +308,5 @@ def history():
 
 # ---------- RUN ----------
 if __name__ == "__main__":
-    app.run(
-        debug=True,
-        host="127.0.0.1",   # Explicit localhost binding (FIX)
-        port=5000
-    )
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
